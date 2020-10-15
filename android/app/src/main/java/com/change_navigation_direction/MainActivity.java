b0a28267a343d555d6c9196d20d4bb26fcd1ffd4
Justyna Sowinska <jsowinska@eworklabs.io>
2020-10-15 19:17:01 +0200

    initial commit

---
+++ android/app/src/main/java/com/change_navigation_direction/MainActivity.java
@@ -0,0 +1,15 @@
+package com.change_navigation_direction;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "change_navigation_direction";
+  }
+}
